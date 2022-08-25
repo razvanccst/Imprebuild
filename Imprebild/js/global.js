c6cb3a21561ab33255f00c08bde0e929103d523d
Razvan <razvanccst@users.noreply.github.com>
2022-08-25 17:08:20 +0300

    v!.0.3

--- Imprebild/js/global.js
+++ Imprebild/js/global.js
@@ -723,6 +723,8 @@ window.onscroll = function () {
   scrollFunction();
 };
 
+
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
